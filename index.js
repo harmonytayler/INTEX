987be035b8726d166e7a059068ef0d20200ca446
akyoungbyu <156359016+akyoungbyu@users.noreply.github.com>
2024-12-03 12:33:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -35,137 +35,58 @@ const knex = require("knex")({
     }
 });
 
-app.get("/", (req, res) => {
-    res.render("landing1");
-});
-
-app.get("/login", (req, res) => {
-    res.render("login");
-});
-
-app.get("/about", (req, res) => {
-    res.render("about");
-});
-
-app.get("/request", (req, res) => {
-    res.render("request");
-});
-
-app.post("/request", (req, res) => {
-    //post information for updating the pending events table
-});
-
-app.get("/volunteer", (req, res) => {
-    res.render("volunteer");
-});
-
-app.post("/volunteer", (req, res) => {
-    //post information for updating the volunteer table
-});
-
-app.get("/donate", (req, res) => {
-    res.render("donate");
-});
-
-app.get("/addPast", (req, res) => {
-    res.render("addPast");
-});
-
-app.post("/addPast", (req, res) => {
-    //post information for updating events table (status:completed)
-});
-
-app.get("/addRequest", (req, res) => {
-    res.render("addRequest");
-});
-
-app.post("/addRequest", (req, res) => {
-    //post information for updating events table (status:pending)
-});
-
-app.get("/addScheduled", (req, res) => {
-    res.render("addScheduled");
-});
-
-app.post("/addScheduled", (req, res) => {
-    //post information for updating events table (status:scheduled)
-});
-
-app.post("/editPast", (req, res) => {
-    //post information for updating events table (status:completed)
-});
-
-app.get("/editRequest", (req, res) => {
-    res.render("editRequest");
-});
-
-app.post("/editRequest", (req, res) => {
-    //post information for updating events table (status:pending)
-});
-
-app.get("/editScheduled", (req, res) => {
-    res.render("editScheduled");
-});
-
-app.post("/editScheduled", (req, res) => {
-    //post information for updating events table (status:scheduled)
-});
-
-app.get("/editUser", (req, res) => {
-    res.render("editUser");
-});
-
-app.post("/editUser", (req, res) => {
-    //post information for updating users table
-});
-
-app.get("/addVolunteer", (req, res) => {
-    res.render("addPast");
-});
-
-app.post("/addVolunteer", (req, res) => {
-    //post information for updating volunteer table
-});
-
-app.get("/editUser", (req, res) => {
-    res.render("editUser");
-});
-
-app.post("/editUser", (req, res) => {
-    //insert information for updating user table
-});
-
-app.get("/editVolunteer", (req, res) => {
-    res.render("editUser");
-});
-
-app.post("/editVolunteer", (req, res) => {
-    //insert information for updating volunteer
-});
-
 // Pre-login pages
 
     //Route to go to landing page 1
+    app.get("/", (req, res) => {
+        res.render("landing1");
+    });
+
+    //Route to get to about page
+    app.get("/about", (req, res) => {
+        res.render("about");
+    });
 
     // Route from landing page 1 to donate
+    app.get("/donate", (req, res) => {
+        res.render("donate");
+    });
 
     // Route from donate to landing page 1
 
     // Route to submit donations
 
     // Route from landing page 1 to volunteer form
+    app.get("/volunteer", (req, res) => {
+        res.render("volunteer");
+    });    
 
     // Route to submit volunteer form
+    app.post("/volunteer", (req, res) => {
+        //post information for updating the volunteer table
+    });
 
     // Route from landing page 1 to request form
+    app.get("/request", (req, res) => {
+        res.render("request");
+    });
 
     // Route to submit request form
+    app.post("/request", (req, res) => {
+        //post information for updating the pending events table
+    });
 
     // Route from landing page 1 to login page
+    app.get("/login", (req, res) => {
+        res.render("login");
+    });
 
 //Login
 
     // Route to submit login and redirect to landing page 2
+    app.get("/login", (req, res) => {
+    res.render("login");
+});
 
 // Users
 
